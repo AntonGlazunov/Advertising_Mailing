4807f2d348f803cdf3fcaad06a42b920819767fe
Anton Glazunov <tagl91@yandex.ru>
2024-08-27 15:41:55 +0300

    add CRUD mailing

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import MailingCreateView, confirm_mailing, MailingListView
+from mailing.views import MailingCreateView, confirm_mailing, MailingListView, MailingUpdateView, MailingDetailView, \
+    MailingDeleteView
 
 app_name = MailingConfig.name
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('', MailingListView.as_view(), name='mailing_list'),
     path('add-mailing', MailingCreateView.as_view(), name='create_mailing'),
     path('confirm-create-mailing', confirm_mailing, name='confirm_create_mailing'),
+    path('update-mailing/<int:pk>', MailingUpdateView.as_view(), name='update_mailing'),
+    path('detail-mailing/<int:pk>', MailingDetailView.as_view(), name='detail_mailing'),
+    path('delete_mailing/<int:pk>', MailingDeleteView.as_view(), name='delete_mailing'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -39,10 +39,25 @@ class MailingListView(ListView):
 class MailingDetailView(DetailView):
     model = Mailing
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        pk_mailing = self.object.pk
+        mailing = Mailing.objects.get(pk=pk_mailing)
+        if mailing.periodicity == 1:
+            context_data['periodicity'] = 'Ежедневно'
+        elif mailing.periodicity == 2:
+            context_data['periodicity'] = 'Еженедельно'
+        elif mailing.periodicity == 3:
+            context_data['periodicity'] = 'Ежемесячно'
+
+        context_data['subject_text'] = Mail.objects.get(pk=pk_mailing)
+        return context_data
+
 
 class MailingUpdateView(UpdateView):
     model = Mailing
     form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
